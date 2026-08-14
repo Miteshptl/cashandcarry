3281cf8f94675de23cd4b6ff9f159be5aab3b127
Mitesh Gothi <mrp93911@gmail.com>
2026-08-14 13:33:21 +0530

    Deploy Django with Neon PostgreSQL database

--- api/index.py
+++ api/index.py
@@ -1,9 +1,7 @@
-# api/index.py
 import os
 import sys
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
-from django.core.management import call_command
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(ROOT_DIR))
@@ -11,10 +9,3 @@ sys.path.insert(0, str(ROOT_DIR))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = get_wsgi_application()
-
-# Run migrations once on startup if running on Vercel with temp DB
-if os.environ.get('VERCEL'):
-    try:
-        call_command('migrate', interactive=False)
-    except Exception as e:
-        print("Migration error:", e)
--- config/settings.py
+++ config/settings.py
@@ -9,14 +9,6 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
-
-from pathlib import Path
-import os   
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
@@ -27,6 +19,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # DEBUG = True
 
 # ALLOWED_HOSTS = []
+
+from pathlib import Path
+import os   
+import dj_database_url
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
 
 
 # 1. SECURITY CONFIGURATION
@@ -81,13 +83,31 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('/tmp', 'db.sqlite3') if os.environ.get('VERCEL') else os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join('/tmp', 'db.sqlite3') if os.environ.get('VERCEL') else os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
